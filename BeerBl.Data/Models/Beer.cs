35e0dadba4a239092e59de62026f35c812bcbbc0
Ryan Paul <ryanbimmer@gmail.com>
2015-05-14 14:10:13 -0400

    data proj

---
+++ BeerBl.Data/Models/Beer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeerBI.Data
+{
+    public class Beer
+    {
+        public int id { get; set; }
+        public int brewery_id { get; set; }
+        public string name { get; set; }
+        public int cat_id { get; set; }
+        public int style_id { get; set; }
+        public decimal abv { get; set; }
+        public decimal ibu { get; set; }
+        public decimal srm { get; set; }
+        public decimal upc { get; set; }
+        public string filepath { get; set; }
+        public string descript { get; set; }
+        public string add_user { get; set; }
+        public string last_mod { get; set; }
+    }
+}
---
+++ BeerBl.Data/Repository/BeerBlService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerBl.Data
+{
+    public class BeerBlService
+    {
+        #region Beer
+
+
+        #endregion
+
+        #region Brewery
+
+
+
+        #endregion
+
+        #region Category
+
+
+
+        #endregion
+
+        #region Geocode
+
+
+
+        #endregion
+
+        #region Style
+
+
+
+        #endregion
+    }
+}
