4bd2c6f273839f98afb563e2d60b1489b5e9843b
Ryan Paul <ryanbimmer@gmail.com>
2015-05-14 13:43:55 -0400

    RP

---
+++ BeerBl.Data/Repository/DBSingleton.cs
@@ -0,0 +1,31 @@
+﻿namespace BeerBl.Data
+{
+    public sealed class DBSingleton
+    {
+        private static readonly DBSingleton instance = new DBSingleton();
+        private readonly string con = System.Configuration.ConfigurationManager.ConnectionStrings["BeerConnectionString"].ConnectionString;
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static DBSingleton()
+        {
+        }
+
+        private DBSingleton()
+        {
+        }
+
+        public static DBSingleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public string GetDBConnection()
+        {
+            return con;
+        }
+    }
+}
