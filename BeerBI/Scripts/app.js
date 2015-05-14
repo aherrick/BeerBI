a6f7451fb0c4145bb1f8a7ae2c2eab45170106d5
Andrew Herrick <aherrick@indiana.edu>
2015-05-14 11:51:52 -0400

    send data type

--- BeerBI/Scripts/app.js
+++ BeerBI/Scripts/app.js
@@ -3,12 +3,15 @@
 
 $(document).on('change', '.btn-file :file', function () {
 
-    debugger;
     var data = new FormData();
     var files = $(this).get(0).files;
 
     if (files.length > 0) {
         data.append("UploadedBeers", files[0]);
+
+        debugger;
+        var dataType = $("#beerbi-tabs li.active a").text();
+        data.append("DataType", dataType);
     }
 
     var ajaxRequest = $.ajax({
